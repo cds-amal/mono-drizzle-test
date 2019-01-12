3b387f4957d06c6c376cd856545fb73dc958701e
Amal Sudama <cds.sudama@gmail.com>
2019-01-12 00:59:30 -0500

    whoops

--- packages/drizzle/jest.config.js
+++ packages/drizzle/jest.config.js
@@ -119,7 +119,6 @@ module.exports = {
 
   // The path to a module that runs some code to configure or set up the testing framework before each test
   // setupTestFrameworkScriptFile: null,
-  setupTestFrameworkScriptFile: 'jest-generator',
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
